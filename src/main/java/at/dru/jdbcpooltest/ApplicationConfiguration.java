362ed3b9e0a25350a350e5dcea766d3cd778ea66
dru1 <dru1441@gmail.com>
2018-02-05 09:37:56 +0100

    tomcat & css

--- src/main/java/at/dru/jdbcpooltest/ApplicationConfiguration.java
+++ src/main/java/at/dru/jdbcpooltest/ApplicationConfiguration.java
@@ -10,11 +10,25 @@ import javax.sql.DataSource;
 @Configuration
 public class ApplicationConfiguration {
 
+    private static final String JDBC_DRIVER = "org.hsqldb.jdbc.JDBCDriver";
+
     @Bean
     public DataSource dataSource() {
+        loadJDBCDriver();
         // Examines both filesystem and classpath for .properties file
         HikariConfig config = new HikariConfig("/hikari.properties");
         return new HikariDataSource(config);
+    }
+
+    /**
+     * For Tomcat Deployment...
+     */
+    private void loadJDBCDriver() {
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Cannot load driver");
+        }
     }
 }
 
